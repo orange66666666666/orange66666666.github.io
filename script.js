7cc78760f75cdc66d286be9904a68d68832139da
orange66666666666 <80297791+orange66666666666@users.noreply.github.com>
2021-04-16 20:12:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var x="脂";
+var y;
+
+function fun(){
+  y=document.getElementById("A").value;
+  if(y==x){
+    alert("恭喜您答对了");}
+else{alert("答错了答错了")};
+}
+var o="一夜又一夜，猜一个字";
+var p=document.getElementById("A").value;
+
+function funny(){document.getElementById("B").innerHTML="一夜又一夜，猜一个字";
+x="多";
+}
